15da6fb3ff1f31b415f7a3925f529c7a734ff58d
Dario del Rio <57421573+ddelrio95@users.noreply.github.com>
2021-12-08 11:47:52 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'ddelrio95', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
         limit: 8, // How many projects to display.
         exclude: {
@@ -10,8 +10,8 @@ module.exports = {
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
+        linkedin: 'dariodelrio',
+        twitter: 'ddelrio_95',
         facebook: '',
         dribbble: '',
         behance: '',
@@ -23,59 +23,60 @@ module.exports = {
     },
     skills: [
         'PHP',
-        'Laravel',
         'JavaScript',
-        'React.js',
-        'Vue.js',
+        'React.js',     
         'Node.js',
         'Jquery',
         'MySQL',
         'Git',
         'Docker',
-        'CSS',
-        'Antd',
+        'HTML'
+        'CSS',       
         'Tailwind',
         'Bootstrap',
+        'MongoDB'
+        'Python'
+        'DJango'
+        'Java'
+        'C++'
+        'C#'
+        
+        
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Backend Engineer II',
-            from: 'September 2021',
+            company: 'Dario del Rio',
+            position: 'Freelancer',
+            from: '2017',
             to: 'Present'
         },
         { 
-            company: 'Orangetoolz',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
+            company: 'Live and let live pharmacy',
+            position: 'Data Analyst Manager',
+            from: 'July 2017',
+            to: 'Present'
         },
         { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
+            company: 'Keiser University',
+            position: 'QA Tester',
+            from: 'January 2016',
+            to: '2019'
         }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
+            institution: 'Kaiser University',
             degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            from: '2018',
+            to: 'Present'
         },
         { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
+            institution: 'IPI Armando de la Rosa Ruiz-Cuba',
+            degree: 'Associate in IT',
+            from: '2010',
+            to: '2013',
         },
-        { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
-        }
+        
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
@@ -84,12 +85,10 @@ module.exports = {
         limit: 2 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
-        // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id or keep it empty
+         // Please remove this and use your own tag id or keep it empty
     },
     hotjar: {
-        id: '2617601', //  Please remove this and use your own id or keep it empty
-        snippetVersion : 6
+         //  Please remove this and use your own id or keep it empty
     },
     themeConfig: {
         default: 'light',
